022793f9b64273501931693c43985ad04889e983
bliu <brianliu03@gmail.com>
2023-05-24 19:32:13 -0400

    Work on Google Auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import '@styles/globals.css';
 
 import Nav from '@components/Nav';
+import Provider from '@components/Provider';
 
 export const metadata = {
   title: 'AI Coach',
@@ -11,14 +12,16 @@ function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <div className="main">
-          <div className="gradient" />
-        </div>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
 
-        <main className="app">
-          <Nav />
-          {children}
-        </main>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
       </body>
     </html>
   );
