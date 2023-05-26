dba5369da03cd7071261fa699d1957a13c9ebbc3
bliu <brianliu03@gmail.com>
2023-05-25 20:02:32 -0400

    Route is messy, not sure what is going on

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -11,36 +11,43 @@ const handler = NextAuth({
     }),
   ],
 
-  // async session({ session }) {
-  // },
-  // // every nextjs route is a serverless function, lambda function
-  // // that opens up only when it is called, so server does not have
-  // // to run constantly
-  // async signIn({ profile }) {
-  //   try {
-  //     const sessionUser = await prisma.session.findUnique({
-  //       where: {
-  //         email:
-  //     // const user = await prisma.user.findUnique({
-  //     //   where: {
-  //     //     email: profile.email as string,
-  //     //   },
-  //     // });
-  //     // if (user) {
-  //     //   return true;
-  //     // }
-  //     // await prisma.user.create({
-  //     //   data: {
-  //     //     email: profile.email as string,
-  //     //     name: profile.name as string,
-  //     //     image: profile.image as string,
-  //     //   },
-  //     // });
-  //     return true;
-  //   } catch (error) {
+  async session({ session }) {
+    // store user id from prisma in session
+    const sessionUser = await prisma.session.findUnique({
+      where: {
+        email: session.user.email as string
+      }
+    });
+    session.userId = sessionUser?.id;
+  },
+  // every nextjs route is a serverless function, lambda function
+  // that opens up only when it is called, so server does not have
+  // to run constantly
+  async signIn({ profile }) {
+    try {
+      const sessionUser = await prisma.session.findUnique({
+        where: {
+          email: 
+      // const user = await prisma.user.findUnique({
+      //   where: {
+      //     email: profile.email as string,
+      //   },
+      // });
+      // if (user) {
+      //   return true;
+      // }
+      // await prisma.user.create({
+      //   data: {
+      //     email: profile.email as string,
+      //     name: profile.name as string,
+      //     image: profile.image as string,
+      //   },
+      // });
+      return true;
+    } catch (error) {
 
-  //   }
-  // },
+    }
+  },
 });
 
 export { handler as GET, handler as POST };
