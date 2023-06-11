b5f7c8b45052263d0b90577fa799fbe6fa9f8d74
bliu <brianliu03@gmail.com>
2023-06-11 16:31:51 -0700

    feat: add code

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -1,38 +1,58 @@
-import siteConfig from '@/config/site';
-import cn from '@/lib/utils';
-import { Icons } from '@/components/icons';
+import * as React from "react"
 
-export default function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
+import { siteConfig } from "@/config/site"
+import { cn } from "@/lib/utils"
+import { Icons } from "@/components/icons"
+import { ModeToggle } from "@/components/mode-toggle"
+
+export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
   return (
     <footer className={cn(className)}>
       <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <Icons.logo />
           <p className="text-center text-sm leading-loose md:text-left">
-            Built by
-            {' '}
+            Built by{" "}
             <a
-              href={siteConfig.authors[0].github}
+              href={siteConfig.links.twitter}
               target="_blank"
               rel="noreferrer"
               className="font-medium underline underline-offset-4"
             >
-              {`${siteConfig.authors[0].name} `}
+              shadcn
             </a>
-            and
-            {' '}
+            . Hosted on{" "}
             <a
-              href={siteConfig.authors[1].github}
+              href="https://vercel.com"
               target="_blank"
               rel="noreferrer"
               className="font-medium underline underline-offset-4"
             >
-              {siteConfig.authors[1].name}
+              Vercel
+            </a>
+            . Illustrations by{" "}
+            <a
+              href="https://popsy.co"
+              target="_blank"
+              rel="noreferrer"
+              className="font-medium underline underline-offset-4"
+            >
+              Popsy
+            </a>
+            . The source code is available on{" "}
+            <a
+              href={siteConfig.links.github}
+              target="_blank"
+              rel="noreferrer"
+              className="font-medium underline underline-offset-4"
+            >
+              GitHub
             </a>
             .
           </p>
         </div>
+        <ModeToggle />
       </div>
     </footer>
-  );
+  )
 }
